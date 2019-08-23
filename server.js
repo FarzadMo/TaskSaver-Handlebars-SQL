27aa0c86d695e08626eac1ec1a784956ed66a074
Fred M <farzad.moghbel@gmail.com>
2019-08-23 17:22:05 -0400

    the logic and code are done

--- server.js
+++ server.js
@@ -25,21 +25,26 @@ connection.connect(function(err){
     console.log("the connection id is: "+connection.threadId); 
 });
 
+app.get("/", function(req, res){
 
+    connection.query("SELECT * FROM tasks;", function(err, data){
+        if(err) throw err;
+        console.log('My data is: ', data);
+        res.render("index", {dataObj: data} );
+    });
+});
 
+app.post("/", function(req, res){
+    // var queryData = req.body.task;
+    // console.log(queryData);
+    // console.log("--------------------");
 
+    connection.query("INSERT INTO tasks (task) VALUES (?)", [req.body.task], function(err, data){
+        if(err) throw err;
 
-
-
-
-
-
-
-
-
-
-
-
+        res.redirect("/");
+    });
+});
 
 app.listen(PORT, function(){
     console.log("Server is connected on http://localhost/"+PORT);
